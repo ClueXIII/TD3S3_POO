09e7a6ca622d60ad55145f0a5fe0b92b6870daf4
Quentin <109299545+ClueXIII@users.noreply.github.com>
2023-02-28 15:08:27 +0100

    exo 2

--- main.cpp
+++ main.cpp
@@ -7,9 +7,18 @@ int somme_entiers(int n){
     return n + somme_entiers(n-1);
 }
 
+bool palindrome(char T[], int deb,int fin){
+    if (fin - deb <=0) return true;
+    return T[deb] == T[fin] && palindrome(T, deb + 1, fin - 1);
+}
+
 int main(){
 cout << "------------------------------1------------------------------" << endl;
     cout << somme_entiers(5) << endl;
 cout << "------------------------------2------------------------------" << endl;
+    char tab[]="taat";
+    palindrome(tab,0, 3)? cout << "palindrome"<<endl: cout << "pas palindrome" << endl;
+cout << "------------------------------3------------------------------" << endl;
     
+    return 0;
 }
