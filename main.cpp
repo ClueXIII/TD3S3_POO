d31a27da396f2550f00e75c41cd17543d5b9c9c6
Quentin <109299545+ClueXIII@users.noreply.github.com>
2023-02-28 15:59:19 +0100

    3.2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //la fonction retourne la somme des entiers de 1 à n
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int somme_entiers(int n){
@@ -25,6 +26,25 @@ bool chercherRecur(int T[], int N, int x){
     else chercherRecur(T,N-1,x);
 }
 
+bool chercherDicho(int T[], int N, int x){
+    int deb = 0;
+    int fin = N-1;
+    while (deb<=fin){
+        int m = deb + fin / 2;
+        if (T[m]==x)return true;
+        if (T[m]>x) {
+            deb = m + 1;
+        } else {
+            fin = m - 1;
+        }
+    }
+    return false;
+}
+
+bool chercherDichoRecur(int T[], int N,int x){
+    
+}
+
 int main(){
 cout << "------------------------------1------------------------------" << endl;
     cout << somme_entiers(5) << endl;
@@ -32,9 +52,13 @@ cout << "------------------------------2------------------------------" << endl;
     char tab[]="taat";
     palindrome(tab,0, 3)? cout << "palindrome"<<endl: cout << "pas palindrome" << endl;
 cout << "------------------------------3.0------------------------------" << endl;
-    int tab2[] = {0, 1, 2, 3, 4, 5, 6, 7};
+    int tab2[] = {0,2, 3, 4,7};
     chercher(tab2,3,2)? cout << "le chiffre est bien dans le tableau" << endl : cout << "le chiffre n'est pas dans le tableau" << endl;
 cout << "------------------------------3.1------------------------------" << endl;
     chercherRecur(tab2,3,2)? cout << "le chiffre est bien dans le tableau" << endl : cout << "le chiffre n'est pas dans le tableau" << endl;
+cout << "------------------------------3.2------------------------------" << endl;
+    chercherDicho(tab2,5,5)? cout << "le chiffre est bien dans le tableau" << endl : cout << "le chiffre n'est pas dans le tableau" << endl;
+cout << "------------------------------3.3------------------------------" << endl;
+
     return 0;
 }
